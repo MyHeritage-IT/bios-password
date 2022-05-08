9b3c66fd44151cb581ed1b8dea94b5abe13ce6bb
Sebastian Sejzer <ssejzer@techzer.com.ar>
2022-05-08 16:01:18 +0300

    Add files via upload

--- password.cmd
+++ password.cmd
@@ -16,43 +16,43 @@ if %ERRORLEVEL% EQU 0 GOTO HP
 goto ERROR
 
 :DELL
-echo Dell system detected
+echo %COMPUTERNAME% Dell system detected
 FOR /F %%i IN ('powershell -ExecutionPolicy Bypass .\dell_check.ps1') DO set R=%%i
 if !R! == False (
 	powershell -ExecutionPolicy Bypass .\dell-bios-password.ps1 -NoUserPrompt -AdminSet -AdminPassword !newpassword!
 
 	FOR /F %%i IN ('powershell -ExecutionPolicy Bypass .\dell_check.ps1') DO set R=%%i
-	if !R! == False (echo Failed to set the password) else (echo New password set)
+	if !R! == False (echo %COMPUTERNAME% Failed to set the password) else (echo %COMPUTERNAME% New password set)
 ) else (
-	echo password is already set
+	echo %COMPUTERNAME% password is already set
 )
 goto END
 
 :LENOVO
-echo Lenovo system detected. Lenovo doesn't support setting a new password for the first time.
+echo %COMPUTERNAME% Lenovo system detected. Lenovo doesn't support setting a new password for the first time.
 FOR /F %%i IN ('powershell "(Get-WmiObject -Namespace root\wmi -Class Lenovo_BiosPasswordSettings).PasswordState"') DO set R=%%i
 if !R! == 0 (
-	echo password is not set
+	echo %COMPUTERNAME% password is not set
 
 	powershell -ExecutionPolicy Bypass .\lenovo-bios-password.ps1 -NoUserPrompt -SupervisorSet -SupervisorPassword !newpassword! -OldSupervisorPassword ""
 
 	FOR /F %%i IN ('powershell "(Get-WmiObject -Namespace root\wmi -Class Lenovo_BiosPasswordSettings).PasswordState"') DO set R=%%i
-	if !R! == 0 (echo Failed to set the password) else (echo New password set)
+	if !R! == 0 (echo %COMPUTERNAME% Failed to set the password) else (echo %COMPUTERNAME% New password set)
 ) else (
-	echo password is already set
+	echo %COMPUTERNAME% password is already set
 )
 goto END
 
 :HP
-echo HP system detected
+echo %COMPUTERNAME% HP system detected
 FOR /F %%i IN ('powershell -ExecutionPolicy Bypass .\hp_check.ps1') DO set R=%%i
 if !R! == 0 (
 	powershell -ExecutionPolicy Bypass .\hp-bios-password.ps1 -NoUserPrompt -SetupSet -SetupPassword !newpassword!
 
 	FOR /F %%i IN ('powershell -ExecutionPolicy Bypass .\hp_check.ps1') DO set R=%%i
-	if !R! == 0 (echo Failed to set the password) else (echo New password set)
+	if !R! == 0 (echo %COMPUTERNAME% Failed to set the password) else (echo %COMPUTERNAME% New password set)
 ) else (
-	echo password is already set
+	echo %COMPUTERNAME% password is already set
 )
 goto END
 
@@ -61,6 +61,6 @@ echo Usage: password.cmd NEW-PASSWORD
 goto END
 
 :ERROR
-echo Unkown manufacturer. Aborting...
+echo %COMPUTERNAME% Unkown manufacturer. Aborting...
 
 :END
